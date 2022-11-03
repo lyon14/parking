4ed6194853024f3d301f8b002bfd8f9625a2d310
juan yevenes <juan.yevenes@heartbyte.cl>
2022-11-03 17:25:43 -0300

    home listo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,26 @@
 import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonGrid, IonLabel, IonRow, IonCard, IonList, IonItem, IonCol } from "@ionic/react"
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "../../store";
 import { selectUser } from "../../store/users/selectors/SelectUser"
+import { listVentaId } from "../../store/venta/actions/listVentaId";
+import { initVentaState } from "../../store/venta";
+import { SelectListVentaId } from "../../store/venta/selector/SelectListVentaId";
 
 export const Home: React.FC = () => {
 
+    const dispatch = useDispatch<AppDispatch>();
     const user = useSelector(selectUser);
+    
+    useEffect(() => {
+        dispatch(initVentaState());
+        dispatch(listVentaId(user?.id));
+    }, [dispatch, user]);
+
+    const ListaVentas = useSelector(SelectListVentaId);
+
+    const [results, setResults]= useState(ListaVentas);
+
     
     return (
         <IonPage id="main-content">
@@ -25,7 +41,7 @@ export const Home: React.FC = () => {
                         <IonRow>
                             <IonCard class="ion-padding" style={{width:'100%'}}>
                                 <IonRow class="ion-justify-content-center">
-                                    <IonLabel color="tertiary" style={{fontSize:25,fontWeight:600}}>Historial de Pagos:</IonLabel>
+                                    <IonLabel color="tertiary" style={{fontSize:25,fontWeight:600}}>Historial de Pagos</IonLabel>
                                 </IonRow>
                                 <IonRow class="ion-justify-content-center">
                                     <IonList style={{width:'100%'}}>
@@ -40,62 +56,19 @@ export const Home: React.FC = () => {
                                                 <IonLabel>Valor:</IonLabel>
                                             </IonCol>
                                         </IonItem>
-                                        <IonItem>
-                                            <IonCol size="4">
-                                                <IonLabel>24/10/2022</IonLabel>
-                                            </IonCol>
-                                            <IonCol size="4">
-                                                <IonLabel>22ttdo</IonLabel>
-                                            </IonCol>
-                                            <IonCol size="4">
-                                                <IonLabel>$ 20.000</IonLabel>
-                                            </IonCol>
-                                        </IonItem>
-                                        <IonItem>
-                                            <IonCol size="4">
-                                                <IonLabel>24/10/2022</IonLabel>
-                                            </IonCol>
-                                            <IonCol size="4">
-                                                <IonLabel>22ttdo</IonLabel>
-                                            </IonCol>
-                                            <IonCol size="4">
-                                                <IonLabel>$ 20.000</IonLabel>
-                                            </IonCol>
-                                        </IonItem>
-                                        <IonItem>
-                                            <IonCol size="4">
-                                                <IonLabel>24/10/2022</IonLabel>
-                                            </IonCol>
-                                            <IonCol size="4">
-                                                <IonLabel>22ttdo</IonLabel>
-                                            </IonCol>
-                                            <IonCol size="4">
-                                                <IonLabel>$ 20.000</IonLabel>
-                                            </IonCol>
-                                        </IonItem>
-                                        <IonItem>
-                                            <IonCol size="4">
-                                                <IonLabel>24/10/2022</IonLabel>
-                                            </IonCol>
-                                            <IonCol size="4">
-                                                <IonLabel>22ttdo</IonLabel>
-                                            </IonCol>
-                                            <IonCol size="4">
-                                                <IonLabel>$ 20.000</IonLabel>
-                                            </IonCol>
-                                        </IonItem>
-                                        <IonItem>
-                                            <IonCol size="4">
-                                                <IonLabel>24/10/2022</IonLabel>
-                                            </IonCol>
-                                            <IonCol size="4">
-                                                <IonLabel>22ttdo</IonLabel>
-                                            </IonCol>
-                                            <IonCol size="4">
-                                                <IonLabel>$ 20.000</IonLabel>
-                                            </IonCol>
-                                        </IonItem>
-                                        
+                                        {results?.map((locacion, index) => (
+                                            <IonItem key={index}>
+                                                <IonCol size="4">
+                                                    <IonLabel>{(new Date()).toLocaleDateString('es-cl', locacion.createdAt)}</IonLabel>
+                                                </IonCol>
+                                                <IonCol size="4">
+                                                    <IonLabel>{locacion.patente}</IonLabel>
+                                                </IonCol>
+                                                <IonCol size="4">
+                                                    <IonLabel>$ {locacion.pago}</IonLabel>
+                                                </IonCol>
+                                            </IonItem>
+                                        ))}
                                     </IonList>
                                 </IonRow>
                             </IonCard>
